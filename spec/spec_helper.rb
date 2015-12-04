5f1023cafa7ae605825ca3ccefcfe4a258cf5036
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-04 23:39:43 +0100

    Add configuration for database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'database_cleaner'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -42,6 +43,19 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
+  end
+
+  config.before :each do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
   end
 
 # The settings below are suggested to provide a good initial experience
