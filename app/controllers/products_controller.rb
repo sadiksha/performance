bf38651fd95fb6e4d717897221833297b1eb8e0c
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-04 21:40:19 +0100

    Add product actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(params[:product])
+  end
 end
