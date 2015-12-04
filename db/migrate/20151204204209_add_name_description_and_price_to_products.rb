f71a814cbe1c1eef53b4edf14e669ea2c9d08f7d
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-04 22:03:32 +0100

    Add name, description and price field to product

---
+++ db/migrate/20151204204209_add_name_description_and_price_to_products.rb
@@ -0,0 +1,7 @@
+class AddNameDescriptionAndPriceToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :name, :string
+    add_column :products, :description, :text
+    add_column :products, :price, :decimal, :precision => 8, :scale => 2
+  end
+end
