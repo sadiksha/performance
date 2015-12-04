0455a2815b3e285e865f7c092c671d7f02c4b947
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-04 22:24:16 +0100

    Add attributes name, description and price to the model along with tests:

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe Product, "status", type: :model do
+  it "has attributes name, description and price" do
+    product = Product.new(name: "product", description: "This is a product", price: 12.33)
+    expect(product).to have_attributes(name: "product")
+    expect(product).to have_attributes(description: "This is a product")
+    expect(product).to have_attributes(price: 12.33)
+  end
 end
