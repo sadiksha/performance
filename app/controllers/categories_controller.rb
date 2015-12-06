c73c701ecbbd6336410c33181a444dd30069ddaf
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-06 16:34:37 +0100

    Add new and create functionality to category with tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,15 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to categories_path
+    end
+  end
 end
--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -8,4 +8,16 @@ feature "Category" do
 
     expect(page).to have_content "Category"
   end
+
+  scenario "can be created" do
+    visit categories_path
+    click_link "New"
+
+    fill_in "Name", with: "New Category"
+    click_button "Create Category"
+    expect(Category.count).to eq(1)
+
+    category = Category.first
+    expect(category.name).to eq("New Category")
+  end
 end
