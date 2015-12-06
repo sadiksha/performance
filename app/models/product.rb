fe468a564ee97b64efa00990edf8d61a60a3eb6e
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-06 17:01:58 +0100

    Add presence on name and price for product with model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price
+
+  validates :name, :price, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,28 @@ describe Product, "status", type: :model do
     expect(product).to have_attributes(description: "This is a product")
     expect(product).to have_attributes(price: 12.33)
   end
+
+  describe "validates presence of " do
+    it "name" do
+      product = build(:product, price: 10, name: nil)
+      product.save
+
+      expect(product).to_not be_valid
+
+      errors = product.errors.full_messages
+      expect(errors.count).to eq(1)
+      expect(errors.first).to eq("Name can't be blank")
+    end
+
+    it "price" do
+      product = build(:product, price: nil, name: "Product")
+      product.save
+
+      expect(product).to_not be_valid
+
+      errors = product.errors.full_messages
+      expect(errors.count).to eq(1)
+      expect(errors.first).to eq("Price can't be blank")
+    end
+  end
 end
