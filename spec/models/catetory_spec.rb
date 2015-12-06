f4e716fbe3789840f90eb887ea5e79ea5d49f3e2
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-06 17:29:23 +0100

    Add presence of name on category with model test

--- spec/models/catetory_spec.rb
+++ spec/models/catetory_spec.rb
@@ -5,4 +5,17 @@ describe Category, "status", type: :model do
     category = Category.new(name: "category")
     expect(category).to have_attributes(name: "category")
   end
+
+  describe "validates presence of " do
+    it "name" do
+      category = build(:category, name: nil)
+      category.save
+
+      expect(category).to_not be_valid
+
+      errors = category.errors.full_messages
+      expect(errors.count).to eq(1)
+      expect(errors.first).to eq("Name can't be blank")
+    end
+  end
 end
