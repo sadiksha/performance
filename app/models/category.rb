9ef1b7bd890170f0285f68f10d4a154bdb15157f
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2015-12-06 17:53:16 +0100

    Add association of product and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   attr_accessible :name
 
   validates_presence_of :name
+
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price
 
   validates :name, :price, presence: true
+
+  belongs_to :category
 end
